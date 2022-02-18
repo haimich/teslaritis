7e46c72c02a2f58d96d10dae5c855fb5787d4a5d
Michael Müller <haimich@gmx.net>
2022-02-18 17:12:38 +0100

    added logging

--- crawl.js
+++ crawl.js
@@ -26,6 +26,16 @@ async function loadPageContent() {
 
   const page = await browser.newPage();
 
+  // logging
+  page
+    .on('console', message =>
+      console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+    .on('pageerror', ({ message }) => console.log(message))
+    .on('response', response =>
+      console.log(`${response.status()} ${response.url()}`))
+    .on('requestfailed', request =>
+      console.log(`${request.failure().errorText} ${request.url()}`))
+
   console.log("Opening " + url);
 
   await page.goto(url);
