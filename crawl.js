f30ef5f291000e20f0f36469f7bd0e0f49e3b2c8
Michael Müller <haimich@gmx.net>
2022-02-18 14:29:48 +0100

    readded no sandbox mode

--- crawl.js
+++ crawl.js
@@ -20,8 +20,8 @@ async function loadPageContent() {
 
   const browser = await puppeteer.launch({
     'args': [
-      // '--no-sandbox',
-      // '--disable-setuid-sandbox'
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
     ]});
 
   const page = await browser.newPage();
